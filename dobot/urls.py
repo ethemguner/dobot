4c33e730822f7840e806d456c079207e4877175b
dogukanteber <dogukanteber1@hotmail.com>
2021-12-22 00:23:50 +0300

    Move /coins to / -> refactoring

--- dobot/urls.py
+++ dobot/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from coins.views import dashboard
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('coins.urls'))
+    path('', dashboard, name="dashboard"),
 ]
