125d53465d4887c477f927a8d00ba0a28440f02e
Sencer Hamarat <sencer@dopigo.com>
2022-01-03 22:45:49 +0300

    This closes #41
    Signed-off-by: Sencer Hamarat <sencer@dopigo.com>

--- notifications/models.py
+++ notifications/models.py
@@ -1,39 +1,44 @@
+from django.core.validators import MinLengthValidator, EmailValidator, DecimalValidator
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 
 class Subscription(models.Model):
     class Meta:
-        verbose_name = "Subscription"
-        verbose_name_plural = "Subscriptions"
+        verbose_name = _("Subscription")
+        verbose_name_plural = _("Subscriptions")
 
     email = models.EmailField(
+        verbose_name=_("Email subscribed"),
         null=False,
         blank=False,
         max_length=255,
-        verbose_name="Email subscribed"
+        validators=[MinLengthValidator(5), EmailValidator()]
     )
     target_coins = models.ManyToManyField(
         to="coins.Coin",
-        blank=True,
-        verbose_name="Coins targeted"
+        verbose_name=_("Coins targeted"),
+        blank=True
     )
     active = models.BooleanField(
+        verbose_name=_("Is active?"),
         default=True,
-        verbose_name="Is active?"
     )
     lowest_ratio_point = models.DecimalField(
-        verbose_name="Lowest Ratio Point",
+        verbose_name=_("Lowest Ratio Point"),
         max_digits=19,
         decimal_places=11,
         null=True,
-        blank=True
+        blank=True,
+        validators=[DecimalValidator(19, 11)]
     )
     highest_ratio_point = models.DecimalField(
-        verbose_name="Highest Ratio Point",
+        verbose_name=_("Highest Ratio Point"),
         max_digits=19,
         decimal_places=11,
         null=True,
-        blank=True
+        blank=True,
+        validators=[DecimalValidator(19, 11)]
     )
 
     def __str__(self):
@@ -43,26 +48,42 @@ class Subscription(models.Model):
 
         return info
 
+
 class Notification(models.Model):
     class Meta:
-        verbose_name = "Notification"
-        verbose_name_plural = "Notifications"
+        verbose_name = _("Notification")
+        verbose_name_plural = _("Notifications")
 
     subject = models.CharField(
+        verbose_name=_("Subject"),
         max_length=255,
         null=True,
         blank=False,
-        default="Notification from Dobot."
+        default=_("Notification from Dobot."),
+        validators=[MinLengthValidator(3), ]
     )
-    content = models.CharField(
-        max_length=1500,
+    content = models.TextField(
+        verbose_name=_("Content"),
         null=True,
         blank=False
     )
     subscription = models.ForeignKey(
+        verbose_name=_("Subscription"),
         to="notifications.Subscription",
         null=False,
         blank=False,
-        verbose_name="Subscription",
         on_delete=models.CASCADE
     )
+    created = models.DateTimeField(
+        verbose_name=_("Created at"),
+        null=True,
+        blank=False,
+        auto_now_add=True
+    )
+    read = models.BooleanField(
+        verbose_name=_("Has been read"),
+        default=False
+    )
+
+    def __str__(self):
+        return f"{self.subject}"
